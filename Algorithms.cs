5ebb72a74a899997b322208d37ffa834f05ce7ca
jiriv <jiriv@DESKTOP-C49L7O5>
2023-09-28 20:40:41 +0200

    Přidejte soubory projektu.

---
+++ Algorithms.cs
@@ -0,0 +1,36 @@
+﻿namespace RandomAlgos;
+
+public class Algorithms {
+    public bool LuhnValidation(string cardNum) {
+        List<char> cardSplit = new(cardNum);
+
+        if (cardSplit.Count % 2 == 0) {
+            cardSplit.Insert(0, '0');
+        }
+
+        for (int i = 1; i < cardSplit.Count; i += 2) {
+            int num = (int)char.GetNumericValue(cardSplit[i]);
+            num = num * 2;
+
+            if (num < 10) {
+                cardSplit[i] = (char)(num + '0');
+                continue;
+            }
+
+            int normalized = 0;
+            while (num > 0) {
+                normalized += num % 10;
+                num /= 10;
+            }
+
+            cardSplit[i] = (char)(normalized + '0');
+        }
+
+        int res = 0;
+        for (int i = 0; i < cardSplit.Count; i++) {
+            res += (int)char.GetNumericValue(cardSplit[i]);
+        }
+
+        return res % 10 == 0;
+    }
+}
